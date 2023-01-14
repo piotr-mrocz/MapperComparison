de8e67737f4b32214c020a056c74bb5ef517aded
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-01-15 00:32:30 +0100

    Add models to use it to map objects

---
+++ MapperComparison/MapperComparison/Models/UserToMap.cs
@@ -0,0 +1,11 @@
+﻿namespace MapperComparison.Models;
+public sealed class UserToMap
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LasName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string CardNumber { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+}
