e17f896371636cd6bceb7c1454c8a2b6d29f8ff0
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-01-15 00:40:32 +0100

    Create AutoMapper profile

---
+++ MapperComparison/MapperComparison/AutoMapperProfile/UserProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using MapperComparison.Models;
+
+namespace MapperComparison.AutoMapperProfile;
+public sealed class UserProfile : Profile
+{
+	public UserProfile()
+	{
+		CreateMap<UserToMap, User>().ReverseMap();
+	}
+}
