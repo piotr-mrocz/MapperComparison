cdbb5416b84c566f47d188bb9bae3c1e618b75b1
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-01-15 00:49:13 +0100

    Add method to map list of objects manually

--- MapperComparison/MapperComparison/Services/MapService.cs
+++ MapperComparison/MapperComparison/Services/MapService.cs
@@ -17,5 +17,24 @@ public static class MapService
         };
     }
 
+    public static List<User> MapListOfObjectsManually(List<UserToMap> usersToMap)
+    {
+        var listToReturn = new List<User>();
 
+        foreach (var userToMap in usersToMap)
+        {
+            listToReturn.Add(new User()
+            {
+                Id = userToMap.Id,
+                FirstName = userToMap.FirstName,
+                LasName = userToMap.LasName,
+                Email = userToMap.Email,
+                CardNumber = userToMap.CardNumber,
+                Password = userToMap.Password,
+                PhoneNumber = userToMap.PhoneNumber
+            });
+        }
+
+        return listToReturn;
+    }
 }
