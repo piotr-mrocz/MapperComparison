fc85c2fde147ea0c639e96923ac45cdbbd45c583
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-01-15 02:01:54 +0100

    Add implicit operator

--- MapperComparison/MapperComparison/Models/User.cs
+++ MapperComparison/MapperComparison/Models/User.cs
@@ -8,4 +8,19 @@ public sealed class User
     public string Password { get; set; } = null!;
     public string CardNumber { get; set; } = null!;
     public string PhoneNumber { get; set; } = null!;
+
+
+    public static implicit operator User(UserToMap userToMap)
+    {
+        return new User()
+        {
+            Id = userToMap.Id,
+            FirstName = userToMap.FirstName,
+            LasName = userToMap.LasName,
+            Email = userToMap.Email,
+            CardNumber = userToMap.CardNumber,
+            Password = userToMap.Password,
+            PhoneNumber = userToMap.PhoneNumber
+        };
+    }
 }
