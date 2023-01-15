e582ef69d3ad547d29d8d94e29abf88c986f9fad
Piotr Mroczkowski <p.mrocz@vp.pl>
2023-01-15 01:04:25 +0100

    Add static class to initialize IMapper

---
+++ MapperComparison/MapperComparison/Services/AutoMapperInitializator.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace MapperComparison.Services;
+public static class AutoMapperInitializator
+{
+    public static IMapper GetMapper(MapperConfiguration config)
+        => config.CreateMapper();
+}
